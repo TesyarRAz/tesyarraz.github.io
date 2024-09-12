096ad386e3c802751ac16caf0fc30b077e5e3a8c
TesyarRAz <mohamadtesyar@gmail.com>
2024-09-12 23:19:52 +0700

    update

--- script.js
+++ script.js
@@ -1,36 +1,40 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const curtab = window.location.hash
+document.addEventListener("DOMContentLoaded", () => {
+  const curtab = window.location.hash;
 
-    if (!curtab) {
-        window.location.href = `#home`;
-    }
+  if (!curtab) {
+    window.location.href = `#home`;
+  }
 
-    const tabs = {
-        '#home': '#home-tab',
-        '#projects': '#projects-tab',
-    }
+  const tabs = {
+    "#home": "#home-tab",
+    "#projects": "#projects-tab",
+  };
 
-    const setActiveTab = (tab) => {
-        Object.keys(tabs).forEach(t => {
-            if (t == tab) {
-                document.querySelector(tabs[t]).style.display = 'flex';
-                return
-            }
+  const setActiveTab = (tab) => {
+    Object.keys(tabs).forEach((t) => {
+      if (t == tab) {
+        console.log(t, tab);
+        document.querySelector(t).classList.add("active");
+        document.querySelector(tabs[t]).style.display = "flex";
+      } else {
+        document.querySelector(t).classList.remove("active");
+        document.querySelector(tabs[t]).style.display = "none";
+      }
+    });
+  };
 
-            document.querySelector(tabs[t]).style.display = 'none';
-        })
-    }
+  Object.keys(tabs).forEach((tab) => {
+    const tabElement = document.querySelector(tab);
+    tabElement.addEventListener("click", (e) => {
+      setActiveTab(tab);
+    });
+  });
 
-    Object.keys(tabs).forEach(tab => {
-        const tabElement = document.querySelector(tab);
-        tabElement.addEventListener('click', () => {
-            setActiveTab(tab)
-        })
+  document.querySelectorAll("[data-trigger=tab]").forEach((e) =>
+    e.addEventListener("click", (e) => {
+      setActiveTab(e.target.getAttribute("data-target"));
     })
+  );
 
-    document.querySelectorAll("[data-trigger=tab]").forEach(e => e.addEventListener('click', (e) => {
-        setActiveTab(e.target.href)
-    }))
-
-    setActiveTab(curtab)
-})
+  setActiveTab(curtab);
+});
